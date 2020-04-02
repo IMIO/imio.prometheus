4d1af9cdaf8a425779106e85817276b880f472c0
Benoit Suttor <bsuttor@imio.be>
2020-04-02 15:48:53 +0200

    Preparing release 1.0a4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.prometheus",
-    version="1.0a4.dev0",
+    version="1.0a4",
     description="Add some iMio informations on prometheus metrics view",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
