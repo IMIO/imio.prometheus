342e68236ee331d85293e66148425f4cf374f62d
Benoit Suttor <benoit.suttor@imio.be>
2022-02-01 09:42:02 +0100

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.prometheus",
-    version="1.0.1",
+    version="1.0.2.dev0",
     description="Add some iMio informations on prometheus metrics view",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
